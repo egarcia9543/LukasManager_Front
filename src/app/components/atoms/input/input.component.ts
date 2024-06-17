b0c50631b018902ac93ea9b0ca624f38523ec3ee
egarcia9543 <egarcia9543@soy.sena.edu.co>
2024-06-16 23:26:15 -0500

    feat: create signup page and functionality

--- src/app/components/atoms/input/input.component.ts
+++ src/app/components/atoms/input/input.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { MatFormField, MatInputModule } from '@angular/material/input'
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatFormField
+  ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss'
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
+  @Input() inputType: string = '';
+  @Input() placeholder: string = '';
+  @Input() label: string = '';
 
+  public onChange: any = () => {};
+
+  public onTouched: any = () => {};
+
+  public writeValue(value: string): void {
+    this.onChange(value);
+  }
+
+  public registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  updateValue(event: Event): void {
+    this.onChange((event.target as HTMLInputElement).value);
+  }
 }
